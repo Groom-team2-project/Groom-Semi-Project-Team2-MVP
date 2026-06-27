2e278fb5784ad956f99c9ab58b9067d189803145
Hi0Park <pgi668.YWP@gmail.com>
2026-06-27 13:11:07 +0900

    chore: 프로젝트 초기 세팅 및 구조 생성

---
+++ src/main/java/org/example/groommvp/GroomMvpApplication.java
@@ -0,0 +1,13 @@
+package org.example.groommvp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroomMvpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroomMvpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/groommvp/GroomMvpApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.groommvp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroomMvpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
