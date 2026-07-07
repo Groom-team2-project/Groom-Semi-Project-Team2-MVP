484b7a771a6044a3aa7c197c638559b1f820603c
mmy428 <mmy428@naver.com>
2026-07-07 23:11:21 +0900

    dto import 수정

--- src/main/java/org/example/groommvp/domain/product/dto/ProductCreateRequest.java
+++ src/main/java/org/example/groommvp/domain/product/dto/ProductCreateRequest.java
@@ -1,9 +1,6 @@
 package org.example.groommvp.domain.product.dto;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
